dda73367157151c3f114ef67711b2f5935d1c12a
carol <carolbrandes23@gmail.com>
2023-07-18 08:48:15 -0300

    remove user-agent in api file

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,6 @@ import axios from "axios";
 export const api = axios.create({
   baseURL: "https://oauth.reddit.com",
   headers: {
-    "User-Agent": "ChangeMeClient/0.1 by YourUsername",
     Authorization: `bearer ${import.meta.env.VITE_TOKEN}`,
   },
 });
